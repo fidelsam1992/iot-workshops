ddf9dea4d3fe46fc47f1e07024c9819d46fdf7f0
Mohamed <Omar.Mohamed1@emc.com>
2018-06-23 11:30:38 +0200

    updating the master code for handling the case for all 4 sensors

--- projects/parking/code/master_node.ino
+++ projects/parking/code/master_node.ino
@@ -1,33 +1,20 @@
-/*
- Basic ESP8266 MQTT example
- This sketch demonstrates the capabilities of the pubsub library in combination
- with the ESP8266 board/library.
- It connects to an MQTT server then:
-  - publishes "hello world" to the topic "outTopic" every two seconds
-  - subscribes to the topic "inTopic", printing out any messages
-    it receives. NB - it assumes the received payloads are strings not binary
-  - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
-    else switch it off
- It will reconnect to the server if the connection is lost using a blocking
- reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
- achieve the same result without blocking the main loop.
- To install the ESP8266 board, (using Arduino 1.6.4+):
-  - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
-       http://arduino.esp8266.com/stable/package_esp8266com_index.json
-  - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
-  - Select your ESP8266 in "Tools -> Board"
-*/
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <StackArray.h>
+
 // Update these with values suitable for your network.
-const char* ssid = "Vodafone-5AA5";
-const char* password = "76767174";
-const char* mqtt_server = "192.168.8.101";
+const char* ssid = "SSID";
+const char* password = "PASSWORD";
+const char* mqtt_server = "MQTT_SERVER";
+
 WiFiClient espClient;
 PubSubClient client(espClient);
-long lastMsg = 0;
-char msg[50];
-int value = 0;
+
+int counter = 0;
+long lastTimeChecked;
+const long interval = 4000;
+bool sensorFlags[5] = {false, false, false, false, false};
+
 void setup_wifi() {
   delay(10);
   // We start by connecting to a WiFi network
@@ -45,23 +32,56 @@ void setup_wifi() {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 }
+
+
 void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
   Serial.println();
-  // Switch on the LED if an 1 was received as first character
-  if ((char)payload[0] == '1') {
-    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
-    // but actually the LED is on; this is because
-    // it is acive low on the ESP-01)
-  } else {
-    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
-  }
+
+   if (millis() - lastTimeChecked > interval) {
+     sensorFlags[1] = false;
+     sensorFlags[2] = false;
+     sensorFlags[3] = false;
+     sensorFlags[4] = false;
+   }
+   
+   if (!sensorFlags[1] && !sensorFlags[2] && strcmp(topic, "sensor1") == 0) {
+      sensorFlags[1] = true;
+      lastTimeChecked = millis();
+   } else if ((millis() - lastTimeChecked < interval) && sensorFlags[2] && strcmp(topic, "sensor1") == 0) {
+      counter--;
+      long value = 
+      Serial.println(millis() - lastTimeChecked);
+      sensorFlags[2] = false; 
+   } else if (!sensorFlags[1] && !sensorFlags[2] && strcmp(topic, "sensor2") == 0) {
+      sensorFlags[2] = true;
+      lastTimeChecked = millis();
+   } else if ((millis() - lastTimeChecked < interval) && sensorFlags[1] && strcmp(topic, "sensor2") == 0) {
+      counter++;
+      sensorFlags[1] = false; 
+   }
+
+   if (!sensorFlags[3] && !sensorFlags[4] && strcmp(topic, "sensor3") == 0) {
+      sensorFlags[3] = true;
+      lastTimeChecked = millis();
+   } else if ((millis() - lastTimeChecked < interval) && sensorFlags[4] && strcmp(topic, "sensor3") == 0) {
+      counter++;
+      sensorFlags[4] = false;
+   } else if (!sensorFlags[3] && !sensorFlags[4] && strcmp(topic, "sensor4") == 0) {
+      sensorFlags[4] = true;
+      lastTimeChecked = millis();
+   } else if ((millis() - lastTimeChecked < interval) && sensorFlags[3] && strcmp(topic, "sensor4") == 0) {
+      counter--;
+      sensorFlags[3] = false; 
+   }
+
+   Serial.println("Counter so far:");
+   Serial.println(counter);
 }
+
+
 void reconnect() {
   // Loop until we're reconnected
   while (!client.connected()) {
@@ -76,6 +96,8 @@ void reconnect() {
       // ... and resubscribe
       client.subscribe("sensor1");
       client.subscribe("sensor2");
+      client.subscribe("sensor3");
+      client.subscribe("sensor4");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -85,6 +107,8 @@ void reconnect() {
     }
   }
 }
+
+
 void setup() {
   pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
   Serial.begin(115200);
@@ -92,14 +116,12 @@ void setup() {
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
 }
+
 void loop() {
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
   long now = millis();
-  if (now - lastMsg > 2000) {
-    lastMsg = now;
-    ++value;
-  }
+  delay(1000);
 }
